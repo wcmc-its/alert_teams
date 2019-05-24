025fb767c731b1ab76e6fc6a271587521814a5e6
Bryan Fisher <brf2010@med.cornell.edu>
2019-05-24 11:45:02 -0400

    indent

--- bin/teams.py
+++ bin/teams.py
@@ -60,6 +60,7 @@ if __name__ == "__main__":
             # teams uses markdown in the value field but not the name field
             value = escape_markdown(value)
             facts.append({"name":key, "value":value})
+
 		# debug facts order
 		print >> sys.stderr, "INFO Facts: %s" % str(facts)
 
